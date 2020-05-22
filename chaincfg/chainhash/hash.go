796def8d3eaa1c8be8b0e8758421e3ea2079b730
36dge <695931535@qq.com>
2020-05-22 15:33:11 +0800

    hash 算法

--- chaincfg/chainhash/hash.go
+++ chaincfg/chainhash/hash.go
@@ -15,7 +15,7 @@ const MaxHashStringSize = HashSize * 2
 //一个字符太多的字符串
 var ErrHashStrSize = fmt.Errorf("max hash string length is %v bytes", MaxHashStringSize)
 
-//哈希用于比特币消息和常见的结构中，通常表示数据的双sha256
+//哈希用于比特币消息和常见的结构中，通常表示数据的双sha256  Hash相当于一个字节数组类型
 type Hash [HashSize]byte
 
 // String returns the Hash as the hexadecimal string of the byte-reversed
@@ -27,4 +27,102 @@ func (hash Hash) String() string {
 	return hex.EncodeToString(hash[:])
 }
 
+// CloneBytes returns a copy of the bytes which represent the hash as a byte
+// slice.
+//
+// NOTE: It is generally cheaper to just slice the hash directly thereby reusing
+// the same bytes rather than calling this method.
 
+//CloneBytes返回字节的副本，该副本将哈希表示为一个字节。
+//切片。
+//注意：直接切碎散列通常比较便宜，这样可以重复使用
+//相同的字节，而不是调用此方法。
+
+func (hash *Hash) CloneBytes() []byte {
+	newHash := make([]byte, HashSize)
+	copy(newHash, hash[:])
+	return newHash
+}
+
+//setbytes设置表示哈希的字节，一个错误将会返回
+//如果传入的字节数不是hashsize
+func (hash *Hash) SetBytes(newHash []byte) error {
+	nhlen := len(newHash)
+	if nhlen != HashSize {
+		return fmt.Errorf("invalid hash length of %v, want %v", nhlen, HashSize)
+	}
+	copy(hash[:], newHash)
+	return nil
+}
+
+// 如果目标与哈希相同，则isequal返回true
+func (hash *Hash) IsEqual(target *Hash) bool {
+	if hash == nil && target == nil {
+		return true
+	}
+	if hash == nil || target == nil {
+		return false
+	}
+	return *hash == *target
+}
+
+// NewHash returns a new Hash from a byte slice.  An error is returned if
+// the number of bytes passed in is not HashSize.
+
+func NewHash(newHash []byte) (*Hash, error) {
+	var sh Hash
+	err := sh.SetBytes(newHash)
+	if err != nil {
+		return nil, err
+	}
+	return &sh, err
+}
+
+//newhashfromstr从哈希字符串创建哈希。字符串应该是
+//字节反向散列的十六进制字符串，如何缺少任何字符
+//则在散列结尾处使用零填充。
+func NewHashFromStr(hash string) (*Hash, error) {
+	ret := new(Hash)
+	err := Decode(ret, hash)
+	if err != nil {
+		return nil, err
+	}
+	return ret, nil
+}
+
+// Decode decodes the byte-reversed hexadecimal string encoding of a Hash to a
+// destination.
+
+func Decode(dst *Hash, src string) error {
+	//return error if hash string is too long
+	if len(src) > MaxHashStringSize {
+		return ErrHashStrSize
+	}
+	//hex decoder expects the hash to be a multiple of two
+	// when not ,padding with a leading zero
+	var srcBytes []byte
+	if len(src)%2 == 0 {
+		srcBytes = []byte(src)
+	} else {
+		srcBytes = make([]byte, 1+len(src))
+		srcBytes[0] = '0'
+		copy(srcBytes[1:], src)
+	}
+
+	//hex decode the source bytes to temporary destiation
+	var reverseHash Hash
+	_, err := hex.Decode(reverseHash[HashSize-hex.DecodedLen(len(srcBytes)):], srcBytes)
+	if err != nil {
+		return err
+	}
+
+	//reverse copy from the temporary hash to destination ,because the
+	// temporary was zeroed,the written result will be crorectly padded.
+	for i, b := range reverseHash[:HashSize/2] {
+		dst[i], dst[HashSize-1-i] = reverseHash[HashSize-1], b
+	}
+	return nil
+
+}
+
+//over
---
+++ chaincfg/chainhash/hashfuncs.go
@@ -0,0 +1,27 @@
+package chainhash
+
+import "crypto/sha256"
+
+//hash b计算hash(b)并返回结果字节
+func HashB(b []byte) []byte {
+	hash := sha256.Sum256(b)
+	return hash[:]
+}
+
+//hash计算散列 （b）并已散列形式返回结果字节.
+func HashH(b []byte) Hash {
+	return Hash(sha256.Sum256(b))
+}
+
+//doublehashb计算散列（hash(b)）并返回结果字节
+func DoubleHashB(b []byte) []byte {
+	first := sha256.Sum256(b)
+	second := sha256.Sum256(first[:])
+	return second[:]
+}
+
+//doublehash计算哈希（hash(b)）,并将结果字节返回为哈希
+func DoubleHashH(b []byte) Hash {
+	first := sha256.Sum256(b)
+	return Hash(sha256.Sum256(first[:]))
+}
