c7f7361981a9150d1d39a6b867aea1adece5f732
36dge <695931535@qq.com>
2020-06-01 20:31:29 +0800

    交易验证

---
+++ blockchain/utxoviewpoint.go
@@ -0,0 +1,54 @@
+package blockchain
+
+import (
+	"BtcoinProject/chaincfg/chainhash"
+	"BtcoinProject/wire"
+)
+
+
+// UtxoEntry houses details about an individual transaction output in a utxo
+// view such as whether or not it was contained in a coinbase tx, the height of
+// the block that contains the tx, whether or not it is spent, its public key
+// script, and how much it pays.
+type UtxoEntry struct {
+	// NOTE: Additions, deletions, or modifications to the order of the
+	// definitions in this struct should not be changed without considering
+	// how it affects alignment on 64-bit platforms.  The current order is
+	// specifically crafted to result in minimal padding.  There will be a
+	// lot of these in memory, so a few extra bytes of padding adds up.
+
+	amount      int64
+	pkScript    []byte // The public key script for the output.
+	blockHeight int32  // Height of block containing tx.
+
+	// packedFlags contains additional info about output such as whether it
+	// is a coinbase, whether it is spent, and whether it has been modified
+	// since it was loaded.  This approach is used in order to reduce memory
+	// usage since there will be a lot of these in memory.
+	packedFlags txoFlags
+}
+
+
+
+
+// UtxoViewpoint represents a view into the set of unspent transaction outputs
+// from a specific point of view in the chain.  For example, it could be for
+// the end of the main chain, some point in the history of the main chain, or
+// down a side chain.
+//
+// The unspent outputs are needed by other transactions for things such as
+// script validation and double spend prevention.
+type UtxoViewpoint struct {
+	entries map[wire.OutPoint]*UtxoEntry
+	bestHash chainhash.Hash
+}
+
+
+// LookupEntry returns information about a given transaction output according to
+// the current state of the view.  It will return nil if the passed output does
+// not exist in the view or is otherwise not available such as when it has been
+// disconnected during a reorg.
+func (view *UtxoViewpoint) LookupEntry(outpoint wire.OutPoint) *UtxoEntry {
+	return view.entries[outpoint]
+}
+
--- mempool/policy.go
+++ mempool/policy.go
@@ -5,6 +5,7 @@ import (
 	"BtcoinProject/wire"
 	"fmt"
 	"github.com/btcsuite/btcutil"
+	"time"
 )
 
 /*
@@ -201,3 +202,230 @@ func checkPkScriptStandard(pkScript []byte, scriptClass txscript.ScriptClass) er
 	return nil
 
 }
+
+/*
+Bitcoin dust refers to the small amount of bitcoin which is lower than
+the minimum limit of a valid transaction.
+
+Bitcoin dust is the relatively smaller amounts of bitcoin lying in a
+particular wallet or address whose monetary value is so tiny that it
+is even lower than the amount of the fee required to spend the bitcoin.
+It makes the transaction impossible to process.
+*/
+
+//isdust returns whethers or not the passed transaction output amount is
+//considered dust or not based on the passed minimum transaction relay fee
+//dust is defined in terms of the minimum transaction relay fee.in particular
+//if the cost to the newwork to spend coins is more than 1/3 of the minimum
+//transaction relay fee. it is considerd dust
+func isDust(txOut *wire.TxOut, minRelayTxFee btcutil.Amount) bool {
+	//unspendable outputs are considered dust.
+	if txscript.IsUnspendable(txOut.PKScript) {
+		return true
+	}
+
+	//the serialized size consists of the output and the associated
+	//input script to redeem it.since there is no input script to
+	//redeem it yet .use the miniumu size fo a typical input script
+
+	//1. pay to pubkey hash bytes breakdown:
+
+	//  Output to hash (34 bytes):
+	//   8 value, 1 script len, 25 script [1 OP_DUP, 1 OP_HASH_160,
+	//   1 OP_DATA_20, 20 hash, 1 OP_EQUALVERIFY, 1 OP_CHECKSIG]
+	//
+	//  Input with compressed pubkey (148 bytes):
+	//   36 prev outpoint, 1 script len, 107 script [1 OP_DATA_72, 72 sig,
+	//   1 OP_DATA_33, 33 compressed pubkey], 4 sequence
+	//
+	//  Input with uncompressed pubkey (180 bytes):
+	//   36 prev outpoint, 1 script len, 139 script [1 OP_DATA_72, 72 sig,
+	//   1 OP_DATA_65, 65 compressed pubkey], 4 sequence
+
+	//2. pay to pubkey bytes breakdown:
+
+	//  Output to compressed pubkey (44 bytes):
+	//   8 value, 1 script len, 35 script [1 OP_DATA_33,
+	//   33 compressed pubkey, 1 OP_CHECKSIG]
+	//
+	//  Output to uncompressed pubkey (76 bytes):
+	//   8 value, 1 script len, 67 script [1 OP_DATA_65, 65 pubkey,
+	//   1 OP_CHECKSIG]
+	//
+	//  Input (114 bytes):
+	//   36 prev outpoint, 1 script len, 73 script [1 OP_DATA_72,
+	//   72 sig], 4 sequence
+
+	//3.pay to witness pubkey hash bytes breakdown:
+
+	//  Output to witness key hash (31 bytes);
+	//   8 value, 1 script len, 22 script [1 OP_0, 1 OP_DATA_20,
+	//   20 bytes hash160]
+	//
+	//  Input (67 bytes as the 107 witness stack is discounted):
+	//   36 prev outpoint, 1 script len, 0 script (not sigScript), 107
+	//   witness stack bytes [1 element length, 33 compressed pubkey,
+	//   element length 72 sig], 4 sequence
+
+	// Theoretically this could examine the script type of the output script
+	// and use a different size for the typical input script size for
+	// pay-to-pubkey vs pay-to-pubkey-hash inputs per the above breakdowns,
+	// but the only combination which is less than the value chosen is
+	// a pay-to-pubkey script with a compressed pubkey, which is not very
+	// common.
+	//
+	// The most common scripts are pay-to-pubkey-hash, and as per the above
+	// breakdown, the minimum size of a p2pkh input script is 148 bytes.  So
+	// that figure is used. If the output being spent is a witness program,
+	// then we apply the witness discount to the size of the signature.
+	//
+	// The segwit analogue to p2pkh is a p2wkh output. This is the smallest
+	// output possible using the new segwit features. The 107 bytes of
+	// witness data is discounted by a factor of 4, leading to a computed
+	// value of 67 bytes of witness data.
+	//
+	// Both cases share a 41 byte preamble required to reference the input
+	// being spent and the sequence number of the input.
+
+	totalSize := txOut.SerializeSize() + 41
+	if txscript.IsWitnessProgram(txOut.PKScript) {
+		totalSize += (107 / blockchain.WitnessScaleFactor)
+	} else {
+		totalSize += 107
+	}
+
+	// The output is considered dust if the cost to the network to spend the
+	// coins is more than 1/3 of the minimum free transaction relay fee.
+	// minFreeTxRelayFee is in Satoshi/KB, so multiply by 1000 to
+	// convert to bytes.
+	//
+	// Using the typical values for a pay-to-pubkey-hash transaction from
+	// the breakdown above and the default minimum free transaction relay
+	// fee of 1000, this equates to values less than 546 satoshi being
+	// considered dust.
+	//
+	// The following is equivalent to (value/totalSize) * (1/3) * 1000
+	// without needing to do floating point math.
+
+	return txOut.Value*1000/(3*int64(totalSize)) < int64(minRelayTxFee)
+
+}
+
+//checkTrasnactionsstrandrd performes a series of cheecks on a transaction to
+//ensure it is a standard transaction. a standard transaction is one that
+//conforms to several additional limiting cases what is considered a snae
+//transaction such as having a version in the supported range,being finalized,
+//conforming to more stringent size constraints having scripts of recongnized
+//forms and not containing dust outputs (those that are so small it costs
+//more to precess them than they are worth).
+func checkTransactionStandard(tx *btcutil.Tx, height int32,
+	medianTimePast time.Time, minRelayTxFee btcutil.Amount,
+	maxTxVersion int32) error {
+
+	//the transaction must be a curently supported version
+	msgTx := tx.MsgTx()
+	if msgTx.Version > maxTxVersion || msgTx.Version < 1 {
+		str := fmt.Sprintf("transaction version %d in not in the"+
+			"valid range of %d-%d", msgTx.Version, 1, maxTxVersion)
+		return txRuleError(wire.RejectNonstandard, str)
+	}
+	//the transaction must be finalized to be standard and therefore
+	//considered for inclusion in a block
+	if !blockchain.IsFinalizedTransaction(tx, height, medianTimePast) {
+		return txRuleError(wire.RejectNonstandard, "transaction is not finalzed")
+	}
+
+	//since extremely large transaction with a lot of inputs can cost almost
+	//as much to process as the sender fees,limits the maximum size of a transaction
+	//this also helps mitigate CPU exhaustion attacks.
+	txWeight := blockchain.GetTransactionWeight(tx)
+	if txWeight > maxStandardTxWeight {
+		str := fmt.Sprintf("weight of transaction %v is larger than max "+
+			"allowed weigth of %v ", txWeight, maxStandardTxWeight)
+		return txRuleError(wire.RejectNonstandard, str)
+	}
+
+	for i, txIn := range msgTx.TxIn {
+
+		//each transaction input signature script must not exceed the
+		//maximum size allowed for a standard transaction. see the
+		//comment on maxstandardsigscriptsize for more details
+		sigScriptLen := len(txIn.SignatureScript)
+		if sigScriptLen > maxStandardSigScriptSize {
+			str := fmt.Sprintf("transaction input %d: signature "+
+				"script size of %d bytes is large than max "+
+				"allowed size of %d bytes", i, sigScriptLen,
+				maxStandardSigScriptSize)
+			return txRuleError(wire.RejectNonstandard, str)
+		}
+
+		//each transaction input signature script must only contain
+		//opcodes which push data onto the stack
+		if !txscript.IsPushOnlyScript(txIn.SignatureScript) {
+			str := fmt.Sprintf("transaction input %d: signature "+
+				"script is not push only", i)
+			return txRuleError(wire.RejectNonstandard, str)
+		}
+	}
+
+
+		//none of the output public key scripts can be a non-standard script or
+		//be dust (expect when the script is a null data script)
+		numNullDataOutputs := 0
+		for i, txOut := range msgTx.TxOut {
+
+			scriptClass := txscript.GetScriptClass(txOut.PkScript)
+			err := checkPkScriptStandard(txOut.PkScript, scriptClass)
+			if err != nil {
+				//attempt to extract a reject code from the error so it can
+				//be retained. when not possible .fall back to a non standard
+				//error .
+				rejectCode := wire.RejectNonstandard
+				if rejCode, found := extractRejectCode(err); found {
+					rejectCode = rejCode
+				}
+				str := fmt.Sprintf("transaction output %d：%v", i, err)
+				return txRuleError(rejectCode, str)
+			}
+
+			//accumulate the number of outputs which only carry data ,for all
+			//other script types .ensure the outut value is not "dust"
+			if scriptClass == txscript.NullDataTy {
+				numNullDataOutputs++
+			} else if isDust(txOut, minRelayTxFee) {
+				str := fmt.Sprintf("transaction output %d: payment "+
+					"of %d is dust", i, txOut.Value)
+				return txRuleError(wire.RejectDust, str)
+			}
+
+		}
+
+		//a standard transaction must not have more than one output script tha
+		//only carried data
+		if numNullDataOutputs > 1 {
+			str := "more than one transaction output in a nulldata script"
+			return txRuleError(wire.RejectNonstandard, str)
+		}
+
+
+	return nil
+}
+
+// GetTxVirtualSize computes the virtual size of a given transaction. A
+// transaction's virtual size is based off its weight, creating a discount for
+// any witness data it contains, proportional to the current
+// blockchain.WitnessScaleFactor value.
+func GetTxVirtualSize(tx *btcutil.Tx) int64 {
+	// vSize := (weight(tx) + 3) / 4
+	//       := (((baseSize * 3) + totalSize) + 3) / 4
+	// We add 3 here as a way to compute the ceiling of the prior arithmetic
+	// to 4. The division by 4 creates a discount for wit witness data.
+	return (blockchain.GetTransactionWeight(tx) + (blockchain.WitnessScaleFactor - 1)) /
+		blockchain.WitnessScaleFactor
+}
+
+//over
+
+
+
+
