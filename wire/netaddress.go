24b15a62551b2a2df3dfcf9a80660a880b0d68a5
36dge <695931535@qq.com>
2020-07-04 17:09:08 +0800

    read and write NetAddress struct

--- wire/netaddress.go
+++ wire/netaddress.go
@@ -1,6 +1,8 @@
 package wire
 
 import (
+	"encoding/binary"
+	"io"
 	"net"
 	"time"
 )
@@ -29,7 +31,7 @@ type NetAddress struct {
 	Timestamp time.Time
 
 	//bitfield which identifies the services supported by the address
-	Service ServiceFlag
+	Services ServiceFlag
 
 	//ip address of the peer
 	IP net.IP
@@ -54,30 +56,103 @@ func (na *NetAddress) AddService(service ServiceFlag) {
 //ip,port,and supported service ,the timestamp is rountded to single second
 //precision .
 func NewNetAddressTimestamp(
-	timestamp time.Time,service ServiceFlag,ip net.IP,port uint16)  *NetAddress {
+	timestamp time.Time, service ServiceFlag, ip net.IP, port uint16) *NetAddress {
 
 	//limit the timestamp to one second precision since the protocol does support better
 	na := NetAddress{
-		Timestamp: time.Unix(timestamp.Unix(),0),
-		Service: service,
-		IP: ip,
-		Port: port,
+		Timestamp: time.Unix(timestamp.Unix(), 0),
+		Service:   service,
+		IP:        ip,
+		Port:      port,
 	}
-	return  &na
+	return &na
 }
-
 
 //newnetaddressipport returns a new netaddress using the provided ip,port ,and
 //supported service with defaults for the remaining fields.
 func NewNetAddressIPPort(ip net.IP, port uint16, services ServiceFlag) *NetAddress {
-	return NewNetAddressTimestamp(time.Now(),services,ip,port)
+	return NewNetAddressTimestamp(time.Now(), services, ip, port)
 }
 
 //newnetaddress returns a new newtaddress using the provided tcp address and
 //supported services with defaults for the remaining fields.
-func NewNetAddress(addr *net.TCPAddr,services ServiceFlag) *NetAddress {
-	return NewNetAddressIPPort(addr.IP,uint16(addr.Port),services)
+func NewNetAddress(addr *net.TCPAddr, services ServiceFlag) *NetAddress {
+	return NewNetAddressIPPort(addr.IP, uint16(addr.Port), services)
 }
+
+//readnetaddress reads an encoded netaddress from r depending on the protocol
+//version and wherher or not the timestamp is included per ts.some messsages
+//like version do not include the timestamp
+func readNetAddress(r io.Reader, pver uint32, na *NetAddress, ts bool) error {
+	var ip [16]byte
+
+	//NOTE;the bitcoin portocol uses a uint32 for the timestamp so it will
+	//stop working somewhere around2106 ,also timestamp was not added until
+	//protocol version >= netaddresstimeversion
+	if ts && pver >= NetAddressTimeVersion {
+		err := readElement(r, (*uint32Time)(&na.Timestamp))
+		if err != nil {
+			return err
+		}
+	}
+
+	err := readElements(r, &na.Services, &ip)
+	if err != nil {
+		return err
+	}
+
+	//sing .bitcion protocol mixes little an big endian.
+	port, err := binarySerializer.Uint16(r, bigEndian)
+	if err != nil {
+		return err
+	}
+
+	*na = NetAddress{
+		Timestamp: na.Timestamp,
+		Services:  na.Services,
+		IP:        net.IP(ip[:]),
+		Port:      port,
+	}
+	return nil
+}
+
+//writenetaddress serializes a netaddress to w depeding on the protocol
+//version and whether or not the timestamp is included per ts. some message
+//like version do not include the timestamp.
+func writeNetAddress(w io.Writer,pver uint32,na *NetAddress,ts bool)error{
+	//note the bitcoin protocol uses a uint32 for the timestamp so it will
+	//stop working somewhere around 2106,also timestop wasnt not added until
+	//until protocol version >= netaddresstimeversion
+	if ts && pver >= NetAddressTimeVersion{
+		err := writeElement(w,uint32(na.Timestamp.Unix()))
+		if err != nil{
+			return  err
+		}
+	}
+
+	//ensure to always write 16 bytes even if the ip is nil
+	var ip [16]byte
+	if na.IP != nil{
+		copy(ip[:],na.IP.To16())
+
+	}
+	err := writeElements(w,na.Services,ip)
+	if err != nil{
+		return err
+	}
+
+	//sinh. bitcoin portocol mixes littel and big endian
+	return binary.Write(w,bigEndian,na.Port)
+
+}
+
+
+//over
+
+
+
+
+
 
 
 
