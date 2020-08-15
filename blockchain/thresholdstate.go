13d61c48db0d9e9418ae235447830ffc9e5ed768
36dge <695931535@qq.com>
2020-08-15 22:15:39 +0800

    versionbits implement

--- blockchain/thresholdstate.go
+++ blockchain/thresholdstate.go
@@ -15,3 +15,32 @@ func (b *BlockChain) IsDeploymentActive(deploymentID uint32) (bool, error) {
 	return state == ThresholdActive, nil
 }
 
+
+
+
+// thresholdConditionChecker provides a generic interface that is invoked to
+// determine when a consensus rule change threshold should be changed.
+type thresholdConditionChecker interface {
+	// BeginTime returns the unix timestamp for the median block time after
+	// which voting on a rule change starts (at the next window).
+	BeginTime() uint64
+
+	// EndTime returns the unix timestamp for the median block time after
+	// which an attempted rule change fails if it has not already been
+	// locked in or activated.
+	EndTime() uint64
+
+	// RuleChangeActivationThreshold is the number of blocks for which the
+	// condition must be true in order to lock in a rule change.
+	RuleChangeActivationThreshold() uint32
+
+	// MinerConfirmationWindow is the number of blocks in each threshold
+	// state retarget window.
+	MinerConfirmationWindow() uint32
+
+	// Condition returns whether or not the rule change activation condition
+	// has been met.  This typically involves checking whether or not the
+	// bit associated with the condition is set, but can be more complex as
+	// needed.
+	Condition(*blockNode) (bool, error)
+}
---
+++ blockchain/versionbits.go
@@ -0,0 +1,197 @@
+package blockchain
+
+import (
+	"BtcoinProject/chaincfg"
+	"math"
+)
+
+const(
+
+	//vblengacyblockversion is the highest legacy block version before the
+	//version bits shceme became active.
+	vbLegacyBlockVersion = 4
+
+	//vbtopbits defines the bits to set in the version to singnal that the
+	//version bits shceme is being uesed.
+	vbTopBits = 0x20000000
+
+	//vbtopmask is the bitmask to use to determine whether or not the version
+	//bits scheme is in use.
+	vbTopMask = 0xe0000000
+
+	//vbnumbits is the total number of bits availbale for use with the version bits cheme.
+	vbNumBits = 29
+
+	//unknownVernumTocheck is the number of pervious blocks to consider when
+	//checking for a threshold of unkonwn block version for the purpose of warning
+	//the user.
+	unknownVerNumToCheck = 100
+
+	//unkonwnVerWarnNum is the thershold of previous blocks that have an
+	//unknown version to use for the purpose of warning the user.
+	unkonwnVerWarnNum = unknownVerNumToCheck / 2
+
+)
+
+//bitconditionchecker provides a thresholdconditioncheckr which can be used to
+//test whether or not a specific bit is set when it is not supposed to be
+//according to the expected version based on the known deployments and the current
+//state of the chain .this is usefun for detcting and warning about unkonwn rule
+//activactions.
+type bitConditionChecker struct {
+	bit uint32
+	chain *BlockChain
+}
+
+
+// Ensure the bitConditionChecker type implements the thresholdConditionChecker
+// interface.
+var _ thresholdConditionChecker = bitConditionChecker{}
+
+
+//bigintime returns the unix timestamp for the median block time after which
+//voting on a rule change starts(at the next window)
+
+//since this implementation checks for unknown rules ,it returns 0 so the rule
+//is always treeated as active.
+
+// This is part of the thresholdConditionChecker interface implementation.
+
+func (c bitConditionChecker) BeginTime() uint{
+	return 0
+}
+
+//endtime returns the unix timestamp for the medtian block time after which an
+//attempted rule change fails if it has not already been locked in or activated.
+
+//since this implementation chencks for unkonwn rules .it retusn the maximum possible
+//timestamp so the rule is always treated as active.
+
+func (c bitConditionChecker) EndTime() uint64{
+	return math.MaxUint64
+
+}
+
+//rulechangeactivationthreshold is the number of blocks for which the condition
+//must be true in order ot lock in a rule change.
+//this implemation retuns the value defined by the chain params the checker
+//is associated with.
+func (c bitConditionChecker) RuleChangeActivationThreshold() uint32{
+	return c.chain.chainParams.RuleChangeActivationThreshold
+}
+
+//minerconfiratminwindow is the number of blocks in each thershold state
+//retarget window
+func (c bitConditionChecker)MinerConfirmationWindow()uint32  {
+	return c.chain.chainParams.MinerConfirmationWindow
+}
+
+//condition returns ture when the specific bit associated with the check is
+//set and it is not supposed to be according to the expected version based on
+//the konwn deplyments and the current state of the chain.
+func (c bitConditionChecker)Condition(node *blockNode)(bool ,error)  {
+	conditionMask := uint32(1) << c.bit
+	version := uint32(node.version)
+	if version&vbTopMask != vbTopBits{
+		return false,nil
+	}
+	if version&conditionMask == 0{
+		return false,nil
+	}
+
+	expectedVersion ,err := c.chain.calcNextBlockVersion(node.parent)
+	if err != nil{
+		return false,err
+	}
+	return uint32(expectedVersion)&conditionMask == 0, nil
+}
+
+//deploymentchecker provides a thersholdconditionchecker which can be used
+//to test a specfic deployment rule.this is required for properly detecting
+//and activating consensus rule change.
+type deploymentChecker struct {
+	deployment *chaincfg.ConsensusDeployment
+	chain *BlockChain
+}
+
+// Ensure the deploymentChecker type implements the thresholdConditionChecker
+// interface.
+var _ thresholdConditionChecker = deploymentChecker{}
+
+// BeginTime returns the unix timestamp for the median block time after which
+// voting on a rule change starts (at the next window).
+//
+// This implementation returns the value defined by the specific deployment the
+// checker is associated with.
+//
+// This is part of the thresholdConditionChecker interface implementation.
+func (c deploymentChecker) BeginTime() uint64 {
+	return c.deployment.StartTime
+}
+
+// EndTime returns the unix timestamp for the median block time after which an
+// attempted rule change fails if it has not already been locked in or
+// activated.
+//
+// This implementation returns the value defined by the specific deployment the
+// checker is associated with.
+//
+// This is part of the thresholdConditionChecker interface implementation.
+func (c deploymentChecker) EndTime() uint64 {
+	return c.deployment.ExpireTime
+}
+
+// RuleChangeActivationThreshold is the number of blocks for which the condition
+// must be true in order to lock in a rule change.
+//
+// This implementation returns the value defined by the chain params the checker
+// is associated with.
+//
+// This is part of the thresholdConditionChecker interface implementation.
+func (c deploymentChecker) RuleChangeActivationThreshold() uint32 {
+	return c.chain.chainParams.RuleChangeActivationThreshold
+}
+
+// MinerConfirmationWindow is the number of blocks in each threshold state
+// retarget window.
+//
+// This implementation returns the value defined by the chain params the checker
+// is associated with.
+//
+// This is part of the thresholdConditionChecker interface implementation.
+func (c deploymentChecker) MinerConfirmationWindow() uint32 {
+	return c.chain.chainParams.MinerConfirmationWindow
+}
+
+
+//condition returns true when the specific bit defined by the deployment
+//associated with the checker is set.
+func (c deploymentChecker)Condition(node *blockNode)(bool ,error){
+	conditionMask := uint32(1) << c.deployment.BitNumber
+	version := uint32(node.version)
+	return (version&vbTopMask == vbTopBits) && (version&conditionMask != 0),
+		nil
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
