5970af9a58f43cd51da2f6c922f1b49e632753e3
36dge <695931535@qq.com>
2020-08-20 17:23:54 +0800

    add the notificaction package

--- blockchain/notifications.go
+++ blockchain/notifications.go
@@ -1,10 +1,12 @@
 package blockchain
 
-
+import "fmt"
 
 // NotificationType represents the type of a notification message.
 type NotificationType int
 
+
+type NotificationCallback func(*Notification)
 
 // Constants for the type of a notification message.
 const (
@@ -22,6 +24,23 @@ const (
 	NTBlockDisconnected
 )
 
+//notificationtypestring is a map of notifaction types back to their
+//constant names for prety printing
+var notificationTypeStrings = map[NotificationType]string{
+	NTBlockAccepted:     "NTBlockAccepted",
+	NTBlockConnected:    "NTBlockConnected",
+	NTBlockDisconnected: "NTBlockDisconnected",
+}
+
+//string returns the notificationtype in human-readable form
+func (n NotificationType)String()string  {
+	if s,ok := notificationTypeStrings[n];ok{
+		return s
+	}
+	return fmt.Sprintf("unknown notification type (%d)",int(n))
+}
+
+
 // Notification defines notification that is sent to the caller via the callback
 // function provided during the call to New and consists of a notification type
 // as well as associated data that depends on the type as follows:
@@ -32,4 +51,49 @@ type Notification struct {
 	Type NotificationType
 	Data interface{}
 }
+
+
+
+// Subscribe to block chain notifications. Registers a callback to be executed
+// when various events take place. See the documentation on Notification and
+// NotificationType for details on the types and contents of notifications.
+func (b *BlockChain) Subscribe(callback NotificationCallback) {
+	b.notificationsLock.Lock()
+	b.notifications = append(b.notifications, callback)
+	b.notificationsLock.Unlock()
+}
+
+// sendNotification sends a notification with the passed type and data if the
+// caller requested notifications by providing a callback function in the call
+// to New.
+func (b *BlockChain) sendNotification(typ NotificationType, data interface{}) {
+	// Generate and send the notification.
+	n := Notification{Type: typ, Data: data}
+	b.notificationsLock.RLock()
+	for _, callback := range b.notifications {
+		callback(&n)
+	}
+	b.notificationsLock.RUnlock()
+}
+
+//over
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
