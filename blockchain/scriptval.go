548a6192d942700e5d8d4adb94b7531d2b637332
36dge <695931535@qq.com>
2020-10-26 13:16:02 +0800

    create txValidaotr struct and implemented teh validatehandler methods

--- blockchain/scriptval.go
+++ blockchain/scriptval.go
@@ -2,14 +2,108 @@ package blockchain
 
 import (
 	"BtcoinProject/wire"
+	"fmt"
 	"github.com/btcsuite/btcutil"
 )
 
 //txvalidateitem holds a transaction along with which input to validate.
 type txValidateItem struct {
 	txInIndex int
-	txIn *wire.TxIn
-	tx *btcutil.Tx
+	txIn      *wire.TxIn
+	tx        *btcutil.Tx
 	sigHashes *txscript.TxSigHashes
 }
+
+//txvalidator provides a type which asynchronously validates transaction
+//inouts. it provides several channels for communication and a processing
+//function that is intended to be in run multiple goroutines.
+type txValidator struct {
+	validateChan chan *txValidateItem
+	quitChan     chan struct{}
+	resultChan   chan error
+	utxoView     *UtxoViewpoint
+	flags        txscript.ScriptFlags
+	sigCache     *txscript.SigCache
+	hashCache    *txscript.HashCache
+}
+
+//sendresult sends the result of a script pair validation on the internal
+//result channel while respecting the quit channel this allows orderly
+//shutdown when the validation process is aborted early due to a valiation
+//error in one of the other goroutines.
+func (v *txValidator) sendResult(result error) {
+	select {
+	case v.resultChan <- result:
+	case <-v.quitChan:
+
+	}
+}
+
+func (v *txValidator) validateHandler() {
+out:
+	for {
+		select {
+		case txVI := <-v.validateChan:
+			//ensure the refered input utxo is avaiable.
+			txIn := txVI.txIn
+			utxo := v.utxoView.LookupEntry(txIn.PreviousOutPoint)
+			if utxo == nil {
+				str := fmt.Sprintf("unable to find unspent"+
+					"output %v referenced from "+
+					"transaction %s :%d",
+					txIn.PreviousOutPoint, txVI.tx.Hash(),
+					txVI.txInIndex)
+				err := ruleError(ErrMissingTxOut, str)
+				v.sendResult(err)
+				break out
+
+			}
+
+			//crate a new script engine for the script pair.
+			sigScript := txIn.SignatureScript
+			witness := txIn.Witness
+			pkScript := utxo.PkScript()
+			inputAmount := utxo.Amount()
+			vm, err := txscript.NewEngine(pkScript, txVI.tx.MsgTx(),
+				txVI.txInIndex, v.flags, v.sigCache, txVI.sigHashes,
+				inputAmount)
+			if err != nil {
+				str := fmt.Sprintf("failed to parse input "+
+					"%s:%d which references output %v - "+
+					"%v (input witness %x, input script "+
+					"bytes %x, prev output script bytes %x)",
+					txVI.tx.Hash(), txVI.txInIndex,
+					txIn.PreviousOutPoint, err, witness,
+					sigScript, pkScript)
+				err := ruleError(ErrScriptMalformed, str)
+				v.sendResult(err)
+				break out
+
+			}
+			//excute the script pair.
+			if err := vm.Execute(); err != nil {
+				str := fmt.Sprintf("failed to validate input "+
+					"%s:%d which references output %v - "+
+					"%v (input witness %x, input script "+
+					"bytes %x, prev output script bytes %x)",
+					txVI.tx.Hash(), txVI.txInIndex,
+					txIn.PreviousOutPoint, err, witness,
+					sigScript, pkScript)
+
+				err := ruleError(ErrScriptMalformed, str)
+				v.sendResult(err)
+				break out
+
+			}
+			//validation succeeded
+			v.sendResult(nil)
+		case <-v.quitChan:
+			break out
+
+		}
+
+	}
+}
+
+
 
