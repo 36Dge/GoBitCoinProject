7eafd334884989a9f5e47394a1d12eb0b64105db
36dge <695931535@qq.com>
2020-11-02 22:34:47 +0800

    difine sequenceLock struct

--- blockchain/chain.go
+++ blockchain/chain.go
@@ -312,3 +312,15 @@ func (b *BlockChain) addOrphanBlock(block *btcutil.Block) {
 	b.prevOrphans[*prevHash] = append(b.prevOrphans[*prevHash], oBlock)
 
 }
+
+
+// SequenceLock represents the converted relative lock-time in seconds, and
+// absolute block-height for a transaction input's relative lock-times.
+// According to SequenceLock, after the referenced input has been confirmed
+// within a block, a transaction spending that input can be included into a
+// block either after 'seconds' (according to past median time), or once the
+// 'BlockHeight' has been reached.
+type SequenceLock struct {
+	Seconds     int64
+	BlockHeight int32
+}
