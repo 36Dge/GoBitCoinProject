7fd8d09ad9007c2c886de9567968f9f9ecb3b422
36dge <695931535@qq.com>
2020-11-08 18:37:35 +0800

    define isBad function

--- addrmgr/knownaddress.go
+++ addrmgr/knownaddress.go
@@ -61,4 +61,46 @@ func (ka *KnownAddress) chance() float64 {
 
 }
 
+//isBad returns ture is the address in question has not been tried in the last
+//minute and meets one of the following criteria:
+// 1) It claims to be from the future
+// 2) It hasn't been seen in over a month
+// 3) It has failed at least three times and never succeeded
+// 4) It has failed ten times in the last week
+// All addresses that meet these criteria are assumed to be worthless and not
+// worth keeping hold of.
+
+func (ka *KnownAddress) isBad()bool{
+	if ka.lastattempt.After(time.Now().Add(-1 * time.Minute)) {
+		return false
+	}
+
+	// From the future?
+	if ka.na.Timestamp.After(time.Now().Add(10 * time.Minute)) {
+		return true
+	}
+
+	// Over a month old?
+	if ka.na.Timestamp.Before(time.Now().Add(-1 * numMissingDays * time.Hour * 24)) {
+		return true
+	}
+
+	// Never succeeded?
+	if ka.lastsuccess.IsZero() && ka.attempts >= numRetries {
+		return true
+	}
+
+	// Hasn't succeeded in too long?
+	if !ka.lastsuccess.After(time.Now().Add(-1*minBadDays*time.Hour*24)) &&
+		ka.attempts >= maxFailures {
+		return true
+	}
+
+	return false
+}
+
+
+
+
+
 
