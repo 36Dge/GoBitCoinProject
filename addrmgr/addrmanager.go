593841adfb7cc83a5a0d2561836b4fa4e7d1c649
36dge <695931535@qq.com>
2020-11-11 22:46:43 +0800

    addmanager serializeaddressmanager struct ...

---
+++ addrmgr/addrmanager.go
@@ -0,0 +1,166 @@
+package addrmgr
+
+import (
+	"BtcoinProject/wire"
+	"container/list"
+	"math/rand"
+	"net"
+	"sync"
+	"time"
+)
+
+//addrmanager provides a concurrency safe address manager for caching potential peers on the bitcoin network.
+type AddrManager struct {
+
+	mtx sync.Mutex
+	peersFile string
+	lookupFunc func(string) ([]net.IP,error)
+	rand           *rand.Rand
+	key            [32]byte
+	addrIndex      map[string]*KnownAddress // address key to ka for all addrs.
+	addrNew        [newBucketCount]map[string]*KnownAddress
+	addrTried      [triedBucketCount]*list.List
+	started        int32
+	shutdown       int32
+	wg             sync.WaitGroup
+	quit           chan struct{}
+	nTried         int
+	nNew           int
+	lamtx          sync.Mutex
+	localAddresses map[string]*localAddress
+	version        int
+}
+
+
+type serializedKnowAddress struct {
+	Addr        string
+	Src         string
+	Attempts    int
+	TimeStamp   int64
+	LastAttempt int64
+	LastSuccess int64
+	Services    wire.ServiceFlag
+	SrcServices wire.ServiceFlag
+	// no refcount or tried, that is available from context.
+}
+
+type serializedAddrManager struct {
+	Version int
+	Key [32]byte
+	Addresses    []*serializedKnownAddress
+	NewBuckets   [newBucketCount][]string // string is NetAddressKey
+	TriedBuckets [triedBucketCount][]string
+}
+
+type localAddress struct {
+	na    *wire.NetAddress
+	score AddressPriority
+}
+
+// AddressPriority type is used to describe the hierarchy of local address
+// discovery methods.
+type AddressPriority int
+
+const (
+	// InterfacePrio signifies the address is on a local interface
+	InterfacePrio AddressPriority = iota
+
+	// BoundPrio signifies the address has been explicitly bounded to.
+	BoundPrio
+
+	// UpnpPrio signifies the address was obtained from UPnP.
+	UpnpPrio
+
+	// HTTPPrio signifies the address was obtained from an external HTTP service.
+	HTTPPrio
+
+	// ManualPrio signifies the address was provided by --externalip.
+	ManualPrio
+)
+
+const (
+	// needAddressThreshold is the number of addresses under which the
+	// address manager will claim to need more addresses.
+	needAddressThreshold = 1000
+
+	// dumpAddressInterval is the interval used to dump the address
+	// cache to disk for future use.
+	dumpAddressInterval = time.Minute * 10
+
+	// triedBucketSize is the maximum number of addresses in each
+	// tried address bucket.
+	triedBucketSize = 256
+
+	// triedBucketCount is the number of buckets we split tried
+	// addresses over.
+	triedBucketCount = 64
+
+	// newBucketSize is the maximum number of addresses in each new address
+	// bucket.
+	newBucketSize = 64
+
+	// newBucketCount is the number of buckets that we spread new addresses
+	// over.
+	newBucketCount = 1024
+
+	// triedBucketsPerGroup is the number of tried buckets over which an
+	// address group will be spread.
+	triedBucketsPerGroup = 8
+
+	// newBucketsPerGroup is the number of new buckets over which an
+	// source address group will be spread.
+	newBucketsPerGroup = 64
+
+	// newBucketsPerAddress is the number of buckets a frequently seen new
+	// address may end up in.
+	newBucketsPerAddress = 8
+
+	// numMissingDays is the number of days before which we assume an
+	// address has vanished if we have not seen it announced  in that long.
+	numMissingDays = 30
+
+	// numRetries is the number of tried without a single success before
+	// we assume an address is bad.
+	numRetries = 3
+
+	// maxFailures is the maximum number of failures we will accept without
+	// a success before considering an address bad.
+	maxFailures = 10
+
+	// minBadDays is the number of days since the last success before we
+	// will consider evicting an address.
+	minBadDays = 7
+
+	// getAddrMax is the most addresses that we will send in response
+	// to a getAddr (in practise the most addresses we will return from a
+	// call to AddressCache()).
+	getAddrMax = 2500
+
+	// getAddrPercent is the percentage of total addresses known that we
+	// will share with a call to AddressCache.
+	getAddrPercent = 23
+
+	// serialisationVersion is the current version of the on-disk format.
+	serialisationVersion = 2
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
