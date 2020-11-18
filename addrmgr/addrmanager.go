696f954b4d7cb181de79aeb93355dd3b40a97093
36dge <695931535@qq.com>
2020-11-18 20:55:21 +0800

    define variables related to addrmanager

--- addrmgr/addrmanager.go
+++ addrmgr/addrmanager.go
@@ -147,6 +147,72 @@ const (
 )
 
 
+const (
+	// needAddressThreshold is the number of addresses under which the
+	// address manager will claim to need more addresses.
+	needAddressThreshold = 1000
+
+	// dumpAddressInterval is the interval used to dump the address
+	// cache to disk for future use.
+	dumpAddressInterval = time.Minute * 10
+
+	// triedBucketSize is the maximum number of addresses in each
+	// tried address bucket.
+	triedBucketSize = 256
+
+	// triedBucketCount is the number of buckets we split tried
+	// addresses over.
+	triedBucketCount = 64
+
+	// newBucketSize is the maximum number of addresses in each new address
+	// bucket.
+	newBucketSize = 64
+
+	// newBucketCount is the number of buckets that we spread new addresses
+	// over.
+	newBucketCount = 1024
+
+	// triedBucketsPerGroup is the number of tried buckets over which an
+	// address group will be spread.
+	triedBucketsPerGroup = 8
+
+	// newBucketsPerGroup is the number of new buckets over which an
+	// source address group will be spread.
+	newBucketsPerGroup = 64
+
+	// newBucketsPerAddress is the number of buckets a frequently seen new
+	// address may end up in.
+	newBucketsPerAddress = 8
+
+	// numMissingDays is the number of days before which we assume an
+	// address has vanished if we have not seen it announced  in that long.
+	numMissingDays = 30
+
+	// numRetries is the number of tried without a single success before
+	// we assume an address is bad.
+	numRetries = 3
+
+	// maxFailures is the maximum number of failures we will accept without
+	// a success before considering an address bad.
+	maxFailures = 10
+
+	// minBadDays is the number of days since the last success before we
+	// will consider evicting an address.
+	minBadDays = 7
+
+	// getAddrMax is the most addresses that we will send in response
+	// to a getAddr (in practise the most addresses we will return from a
+	// call to AddressCache()).
+	getAddrMax = 2500
+
+	// getAddrPercent is the percentage of total addresses known that we
+	// will share with a call to AddressCache.
+	getAddrPercent = 23
+
+	// serialisationVersion is the current version of the on-disk format.
+	serialisationVersion = 2
+)
+
 
 
 
@@ -175,6 +241,7 @@ func (a *AddrManager) getNewBucket(netAddr, srcAddr *wire.NetAddress) int {
 	hash2 := chainhash.DoubleHashB(data2)
 	return int(binary.LittleEndian.Uint64(hash2) % newBucketCount)
 }
+
 
 
 
