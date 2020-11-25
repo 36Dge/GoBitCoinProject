207006fa6110970a97e48166f37c3beb9dddf858
36dge <695931535@qq.com>
2020-11-25 21:49:51 +0800

    implement updataAddress methods

--- addrmgr/addrmanager.go
+++ addrmgr/addrmanager.go
@@ -13,10 +13,9 @@ import (
 
 //addrmanager provides a concurrency safe address manager for caching potential peers on the bitcoin network.
 type AddrManager struct {
-
-	mtx sync.Mutex
-	peersFile string
-	lookupFunc func(string) ([]net.IP,error)
+	mtx            sync.Mutex
+	peersFile      string
+	lookupFunc     func(string) ([]net.IP, error)
 	rand           *rand.Rand
 	key            [32]byte
 	addrIndex      map[string]*KnownAddress // address key to ka for all addrs.
@@ -33,7 +32,6 @@ type AddrManager struct {
 	version        int
 }
 
-
 type serializedKnowAddress struct {
 	Addr        string
 	Src         string
@@ -47,8 +45,8 @@ type serializedKnowAddress struct {
 }
 
 type serializedAddrManager struct {
-	Version int
-	Key [32]byte
+	Version      int
+	Key          [32]byte
 	Addresses    []*serializedKnownAddress
 	NewBuckets   [newBucketCount][]string // string is NetAddressKey
 	TriedBuckets [triedBucketCount][]string
@@ -146,72 +144,84 @@ const (
 	serialisationVersion = 2
 )
 
+//updateaddress is a helper fucntion to either update an address already konwn
+//to the address manager.or to add the address if not already known.
+func (a *AddrManager) updateAddress(netAddr, srcAddr *wire.NetAddress) {
+	//faliter out non-routable address.note that non-routable also inculudes
+	//invalid and local address.
+	if !IsRoutable(netAddr) {
+		return
 
-const (
-	// needAddressThreshold is the number of addresses under which the
-	// address manager will claim to need more addresses.
-	needAddressThreshold = 1000
+	}
 
-	// dumpAddressInterval is the interval used to dump the address
-	// cache to disk for future use.
-	dumpAddressInterval = time.Minute * 10
+	addr := netAddressKey(netAddr)
+	ka := a.find(netAddr)
+	if ka != nil {
+		//todo :only update addresses periodically
+		//update the last seen time and services.
+		//note that to prevent casuing excess garbage on getaddr
+		//message the netaddresses in addrmanager are *immutable*
+		//if we need to change them then we replace the pointer with a
+		//new copy so that we do not have to copy every na for getaddr.
+		if netAddr.Timestamp.After(ka.na.TimeStamp) ||
+			(ka.na.Services&netAddr.Services) !=
+				netAddr.Services {
 
-	// triedBucketSize is the maximum number of addresses in each
-	// tried address bucket.
-	triedBucketSize = 256
+			naCopy := *ka.na
+			naCopy.Timestamp = netAddr.Timestamp
+			naCopy.AddService(netAddr.Services)
+			ka.na = &naCopy
+		}
 
-	// triedBucketCount is the number of buckets we split tried
-	// addresses over.
-	triedBucketCount = 64
+		//if already in tried.we have nothing to do here.
+		if ka.tried {
+			return
+		}
 
-	// newBucketSize is the maximum number of addresses in each new address
-	// bucket.
-	newBucketSize = 64
+		//already at out max?
+		if ka.refs == newBucketsPerAddress {
+			return
+		}
 
-	// newBucketCount is the number of buckets that we spread new addresses
-	// over.
-	newBucketCount = 1024
+		//the more entries we have .the less likely we are to add more.
+		//likelihood is 2N.
+		factor := int32(2 * ka.refs)
+		if a.rand.Int31n(factor) != 0 {
+			return
+		}
 
-	// triedBucketsPerGroup is the number of tried buckets over which an
-	// address group will be spread.
-	triedBucketsPerGroup = 8
+	} else {
+		//make a copy of the net address to avoied races since it is
+		//updated elsewhere in the addremanager code and would otherwise
+		//change the actual netaddress on the peer.
+		netAddrCopy := *netAddr
+		ka = &KnownAddress{na: &netAddrCopy, srcAddr: srcAddr}
+		a.addrIndex[addr] = ka
+		a.nNew++
 
-	// newBucketsPerGroup is the number of new buckets over which an
-	// source address group will be spread.
-	newBucketsPerGroup = 64
+	}
 
-	// newBucketsPerAddress is the number of buckets a frequently seen new
-	// address may end up in.
-	newBucketsPerAddress = 8
+	bucket := a.getNewBucket(netAddr, srcAddr)
 
-	// numMissingDays is the number of days before which we assume an
-	// address has vanished if we have not seen it announced  in that long.
-	numMissingDays = 30
+	//already exists?
+	if _, ok := a.addrNew[bucket][addr]; ok {
+		return
+	}
 
-	// numRetries is the number of tried without a single success before
-	// we assume an address is bad.
-	numRetries = 3
+	//enfore max address .
+	if len(a.addrNew[bucket]) > newBucketSize {
+		log.Tracef("new bucket is full ,expiring old")
+		a.expireNew(bucket)
 
-	// maxFailures is the maximum number of failures we will accept without
-	// a success before considering an address bad.
-	maxFailures = 10
+	}
 
-	// minBadDays is the number of days since the last success before we
-	// will consider evicting an address.
-	minBadDays = 7
+	//add to new bucket
+	ka.refs++
+	a.addrNew[bucket][addr] = ka
 
-	// getAddrMax is the most addresses that we will send in response
-	// to a getAddr (in practise the most addresses we will return from a
-	// call to AddressCache()).
-	getAddrMax = 2500
+	log.Tracef("added new address %s for a total of %d address ", addr, a.nTried+a.nNew)
 
-	// getAddrPercent is the percentage of total addresses known that we
-	// will share with a call to AddressCache.
-	getAddrPercent = 23
-
-	// serialisationVersion is the current version of the on-disk format.
-	serialisationVersion = 2
-)
+}
 
 
 
@@ -241,16 +251,3 @@ func (a *AddrManager) getNewBucket(netAddr, srcAddr *wire.NetAddress) int {
 	hash2 := chainhash.DoubleHashB(data2)
 	return int(binary.LittleEndian.Uint64(hash2) % newBucketCount)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
