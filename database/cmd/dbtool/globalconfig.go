0226c5202fe96f5b0ca7de74152a191700fc72bd
36dge <695931535@qq.com>
2021-02-04 21:58:30 +0800

    fileExiss and validDbType funcitons were implemented

--- database/cmd/dbtool/globalconfig.go
+++ database/cmd/dbtool/globalconfig.go
@@ -4,6 +4,7 @@ import (
 	"BtcoinProject/chaincfg"
 	"BtcoinProject/database"
 	"github.com/btcsuite/btcutil"
+	"os"
 	"path/filepath"
 )
 
@@ -21,9 +22,52 @@ var (
 
 //config defines the global configurtaion options
 type config struct {
-	DataDir string
-	DbType string
-	TestNet3 bool
+	DataDir        string
+	DbType         string
+	TestNet3       bool
 	RegressionTest bool
-	SimNet bool
+	SimNet         bool
 }
+
+//fileexists reports whether the named file or directory exists
+func fileExists(name string) bool {
+	if _, err := os.Start(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+
+	return true
+}
+
+//validdbtype returns whether or not dbtype is a supported database type .
+func validDbType(bdType string) bool {
+	for _, knownType := range knownDbTypes {
+		if dbType == knownType {
+			return true
+		}
+	}
+	return false
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
