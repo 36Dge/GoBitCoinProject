555aaf77a8413aed93554db93f382a19da51e24c
36dge <695931535@qq.com>
2021-02-04 22:01:53 +0800

    netName function was implemented

--- database/cmd/dbtool/globalconfig.go
+++ database/cmd/dbtool/globalconfig.go
@@ -3,6 +3,7 @@ package main
 import (
 	"BtcoinProject/chaincfg"
 	"BtcoinProject/database"
+	"BtcoinProject/wire"
 	"github.com/btcsuite/btcutil"
 	"os"
 	"path/filepath"
@@ -50,24 +51,22 @@ func validDbType(bdType string) bool {
 	return false
 }
 
+// netName returns the name used when referring to a bitcoin network.  At the
+// time of writing, btcd currently places blocks for testnet version 3 in the
+// data and log directory "testnet", which does not match the Name field of the
+// chaincfg parameters.  This function can be used to override this directory name
+// as "testnet" when the passed active network matches wire.TestNet3.
+//
+// A proper upgrade to move the data and log directories for this network to
+// "testnet3" is planned for the future, at which point this function can be
+// removed and the network parameter's name used instead.
 
+func netName(chainParams *chaincfg.Params) string {
+	switch chainParams.Net {
+	case wire.TestNet3:
+		return "testnet"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	default:
+		return chainParams.Name
+	}
+}
