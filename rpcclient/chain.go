66d669eabeb061500f7254db0c5da6dc76652b3b
36dge <695931535@qq.com>
2021-08-25 21:24:39 +0800

    implemeted Receive method of Furturegetblockresult struct

--- rpcclient/chain.go
+++ rpcclient/chain.go
@@ -2,6 +2,9 @@ package rpcclient
 
 import (
 	"BtcoinProject/chaincfg/chainhash"
+	"BtcoinProject/wire"
+	"bytes"
+	"encoding/hex"
 	"encoding/json"
 )
 
@@ -26,3 +29,77 @@ func(r FutureGetBestBlockHashResult) Receive() (*chainhash.Hash,error){
 
 	return chainhash.NewHashFromStr(txHashStr)
 }
+// GetBestBlockHashAsync returns an instance of a type that can be used to get
+// the result of the RPC at some future time by invoking the Receive function on
+// the returned instance.
+//
+// See GetBestBlockHash for the blocking version and more details.
+func (c *Client) GetBestBlockHashAsync() FutureGetBestBlockHashResult {
+	cmd := btcjson.NewGetBestBlockHashCmd()
+	return c.sendCmd(cmd)
+}
+
+// GetBestBlockHash returns the hash of the best block in the longest block
+// chain.
+func (c *Client) GetBestBlockHash() (*chainhash.Hash, error) {
+	return c.GetBestBlockHashAsync().Receive()
+}
+
+// FutureGetBlockResult is a future promise to deliver the result of a
+// GetBlockAsync RPC invocation (or an applicable error).
+type FutureGetBlockResult chan *response
+
+//futuregetblockresult if a future promise to deliver the result of a
+//getblockasyc PRC invocation (or an application error)
+func (r FutureGetBlockResult) Receive() (*wire.MsgBlock,error){
+	res,err := receiveFuture(r)
+	if err != nil {
+		return nil,err
+	}
+
+	//unmarshall result as a string
+	var blockHex string
+	err = json.Unmarshal(res,&blockHex)
+	if err != nil {
+		return nil ,err
+	}
+
+	//decode the serialized block hex to raw bytes.
+	serializedBlock,err := hex.DecodeString(blockHex)
+	if err != nil {
+		return nil,err
+	}
+
+
+	//deserialize the block and return it.
+	var msgBlock wire.MsgBlock
+	err = msgBlock.Deserialize(bytes.NewReader(serializedBlock))
+	if err != nil {
+		return nil ,err
+	}
+	return &msgBlock,nil
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
