2c405c42f5e4c7c3c7f126d610c1268c176c087f
36dge <695931535@qq.com>
2021-09-07 21:26:23 +0800

    define variables

---
+++ rpcclient/notify.go
@@ -0,0 +1,10 @@
+package rpcclient
+
+var (
+	// ErrWebsocketsRequired is an error to describe the condition where the
+	// caller is trying to use a websocket-only feature, such as requesting
+	// notifications or other websocket requests when the client is
+	// configured to run in HTTP POST mode.
+	ErrWebsocketsRequired = errors.New("a websocket connection is required " +
+		"to use this feature")
+)
