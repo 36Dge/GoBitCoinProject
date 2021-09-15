3b57d087f2916816e21f5f3ba912a3219e3c73a9
36dge <695931535@qq.com>
2021-09-15 23:18:45 +0800

    finished Receive function

--- rpcclient/examples/rawrequest.go
+++ rpcclient/examples/rawrequest.go
@@ -1,8 +1,15 @@
 package rpcclient
 
-import "net/http"
+import (
+	"encoding/json"
+)
 
 //futureRawResult is a future promise to deliever the result of a RawRequest PRC
 //invocation (or an applicable error)
 type FutureRawResult chan *response
 
+//receive waits for the response promised by the future and returns the raw
+//response,or an error if the request was unsuccessful.
+func (r FutureRawResult) Receive() (json.RawMessage, error) {
+	return receiveFuture(r)
+}
