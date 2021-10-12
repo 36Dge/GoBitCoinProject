3586fe0f42a4a6f9577b789c3ea662a22fa53bb9
36dge <695931535@qq.com>
2021-10-12 22:15:36 +0800

    the methods relating to list transaxionresult was implemented

--- rpcclient/wallet.go
+++ rpcclient/wallet.go
@@ -1,6 +1,9 @@
 package rpcclient
 
-import "encoding/json"
+import (
+	"BtcoinProject/chaincfg/chainhash"
+	"encoding/json"
+)
 
 //futuregettrnasationresult is a future promise to deliver the result of a gettransactionasync PRC
 //invocation (or an applicabe error )
@@ -24,3 +27,124 @@ func(r FutureGetTransactionResult) Receive() (*btcjson.GetTransactionResult,erro
 
 	return &getTx,nil
 }
+// GetTransactionAsync returns an instance of a type that can be used to get the
+// result of the RPC at some future time by invoking the Receive function on
+// the returned instance.
+//
+// See GetTransaction for the blocking version and more details.
+func (c *Client) GetTransactionAsync(txHash *chainhash.Hash) FutureGetTransactionResult {
+	hash := ""
+	if txHash != nil {
+		hash = txHash.String()
+	}
+
+	cmd := btcjson.NewGetTransactionCmd(hash, nil)
+	return c.sendCmd(cmd)
+}
+
+// GetTransaction returns detailed information about a wallet transaction.
+//
+// See GetRawTransaction to return the raw transaction instead.
+func (c *Client) GetTransaction(txHash *chainhash.Hash) (*btcjson.GetTransactionResult, error) {
+	return c.GetTransactionAsync(txHash).Receive()
+}
+
+// FutureListTransactionsResult is a future promise to deliver the result of a
+// ListTransactionsAsync, ListTransactionsCountAsync, or
+// ListTransactionsCountFromAsync RPC invocation (or an applicable error).
+type FutureListTransactionsResult chan *response
+
+// Receive waits for the response promised by the future and returns a list of
+// the most recent transactions.
+func (r FutureListTransactionsResult) Receive() ([]btcjson.ListTransactionsResult, error) {
+	res, err := receiveFuture(r)
+	if err != nil {
+		return nil, err
+	}
+
+	// Unmarshal result as an array of listtransaction result objects.
+	var transactions []btcjson.ListTransactionsResult
+	err = json.Unmarshal(res, &transactions)
+	if err != nil {
+		return nil, err
+	}
+
+	return transactions, nil
+}
+
+// ListTransactionsAsync returns an instance of a type that can be used to get
+// the result of the RPC at some future time by invoking the Receive function on
+// the returned instance.
+//
+// See ListTransactions for the blocking version and more details.
+func (c *Client) ListTransactionsAsync(account string) FutureListTransactionsResult {
+	cmd := btcjson.NewListTransactionsCmd(&account, nil, nil, nil)
+	return c.sendCmd(cmd)
+}
+
+// ListTransactions returns a list of the most recent transactions.
+//
+// See the ListTransactionsCount and ListTransactionsCountFrom to control the
+// number of transactions returned and starting point, respectively.
+func (c *Client) ListTransactions(account string) ([]btcjson.ListTransactionsResult, error) {
+	return c.ListTransactionsAsync(account).Receive()
+}
+
+// ListTransactionsCountAsync returns an instance of a type that can be used to
+// get the result of the RPC at some future time by invoking the Receive
+// function on the returned instance.
+//
+// See ListTransactionsCount for the blocking version and more details.
+func (c *Client) ListTransactionsCountAsync(account string, count int) FutureListTransactionsResult {
+	cmd := btcjson.NewListTransactionsCmd(&account, &count, nil, nil)
+	return c.sendCmd(cmd)
+}
+
+// ListTransactionsCount returns a list of the most recent transactions up
+// to the passed count.
+//
+// See the ListTransactions and ListTransactionsCountFrom functions for
+// different options.
+func (c *Client) ListTransactionsCount(account string, count int) ([]btcjson.ListTransactionsResult, error) {
+	return c.ListTransactionsCountAsync(account, count).Receive()
+}
+
+// ListTransactionsCountFromAsync returns an instance of a type that can be used
+// to get the result of the RPC at some future time by invoking the Receive
+// function on the returned instance.
+//
+// See ListTransactionsCountFrom for the blocking version and more details.
+func (c *Client) ListTransactionsCountFromAsync(account string, count, from int) FutureListTransactionsResult {
+	cmd := btcjson.NewListTransactionsCmd(&account, &count, &from, nil)
+	return c.sendCmd(cmd)
+}
+
+// ListTransactionsCountFrom returns a list of the most recent transactions up
+// to the passed count while skipping the first 'from' transactions.
+//
+// See the ListTransactions and ListTransactionsCount functions to use defaults.
+func (c *Client) ListTransactionsCountFrom(account string, count, from int) ([]btcjson.ListTransactionsResult, error) {
+	return c.ListTransactionsCountFromAsync(account, count, from).Receive()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
