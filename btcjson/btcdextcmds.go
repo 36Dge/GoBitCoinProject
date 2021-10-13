04fcd1afaaed3d3181cfb3167586e099f109048d
36dge <695931535@qq.com>
2021-10-13 20:43:28 +0800

    creat btcjosn package

---
+++ btcjson/btcdextcmds.go
@@ -0,0 +1,12 @@
+package btcjson
+
+//note:this file is intended to house the PRC commands that are supported by
+//a chain server with btcd extenions.
+
+//nodesubcmd defines the type used in the addnode JSON-RPC command for the
+//sub command field.
+type NodeSubCmd string
+
+const (
+
+)
