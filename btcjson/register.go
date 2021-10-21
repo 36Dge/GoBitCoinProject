3e839937ba805d3aa1fedf006b22dbcaeb5f8fd7
36dge <695931535@qq.com>
2021-10-21 20:43:34 +0800

    define methodInfo struct and four variabls fields

--- btcjson/register.go
+++ btcjson/register.go
@@ -2,13 +2,16 @@ package btcjson
 
 import (
 	"fmt"
+	"reflect"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 //usageflags define flags that specify additional properties about the
 //circumstances under which a command can be used.
 type UsageFlag uint32
+
 const (
 	//ufwalletonly indicates that the command can only be used with an PRC
 	//server that supports wallet commands.
@@ -27,17 +30,17 @@ const (
 	//stingger and tests to ensure all of the above constants have been
 	//tested.
 	highestUsageFlagBit
-
 )
 
 //map of usageflag vulues back to their constant names for prerry printing
-var usageFlagStrings = map[UsageFlag]string {
-	UFWalletOnly: "UFWalletOnly",
+var usageFlagStrings = map[UsageFlag]string{
+	UFWalletOnly:    "UFWalletOnly",
 	UFWebsocketOnly: "UFWebsocketOnly",
-	UFNotification: "UFNotification",
+	UFNotification:  "UFNotification",
 }
+
 //string returns the usagflag in human-readable form.
-func(fl UsageFlag)String() string {
+func (fl UsageFlag) String() string {
 	//no flags are set.
 	if fl == 0 {
 		return "0x0"
@@ -45,43 +48,47 @@ func(fl UsageFlag)String() string {
 
 	//add indivvidual bit falgs.
 	s := ""
-	for flag := UFWalletOnly;flag < highestUsageFlagBit; flag <<= 1 {
-		if fl&flag == flag{
+	for flag := UFWalletOnly; flag < highestUsageFlagBit; flag <<= 1 {
+		if fl&flag == flag {
 			s += usageFlagStrings[flag] + "|"
 			fl -= flag
 		}
 	}
 
 	//add remaining value as raw hex.
-	s = strings.TrimRight(s,"|")
+	s = strings.TrimRight(s, "|")
 	if fl != 0 {
-		s += "|0x" + strconv.FormatUint(uint64(fl),16)
+		s += "|0x" + strconv.FormatUint(uint64(fl), 16)
 	}
-	s = strings.TrimLeft(s,"|")
+	s = strings.TrimLeft(s, "|")
 	return s
 
 }
 
+//methodinfo keeps track of information about each registered method such as
+//the parameter informantion
+type methodInfo struct {
+	maxParams    int
+	numReqParams int
+	numOptParams int
+	defaults     map[int]reflect.Value
+	flags        UsageFlag
+	usage        string
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+var (
+	//this fields are used to map the registed types to method names.
+	registerLock sync.RWMutex
+	methodToConcreteType = make(map[string]reflect.Type)
+	methodToInfo = make(map[string]methodInfo)
+	concreteTypeToMethod = make(map[reflect.Type]string)
+)
 
 
 //mustregistercmd perform the same function as registercmd expect it panics
 //if there is an error. this should only be called form package init functions.
-func MustRegisterCmd (method string ,cmd interface{},flags UsageFlag) {
-	if err := RegisterCmd(method,cmd,flags); err != nil {
-		panic(fmt.Sprintf("failed to register type %q:%v\n",method,err))
+func MustRegisterCmd(method string, cmd interface{}, flags UsageFlag) {
+	if err := RegisterCmd(method, cmd, flags); err != nil {
+		panic(fmt.Sprintf("failed to register type %q:%v\n", method, err))
 	}
 }
