ab8d3d8ebb6e35163c6bf441745cd4b7916ac658
36dge <695931535@qq.com>
2021-12-31 21:55:13 +0800

    define CreateMultisigResult struct

--- btcjson/chainsvrresults.go
+++ btcjson/chainsvrresults.go
@@ -60,27 +60,6 @@ type GetBlockStatusResult struct {
 
 
 
-
-
-
-
-
-
-// GetRawMempoolVerboseResult models the data returned from the getrawmempool
-// command when the verbose flag is set.  When the verbose flag is not set,
-// getrawmempool returns an array of transaction hashes.
-type GetRawMempoolVerboseResult struct {
-	Size             int32    `json:"size"`
-	Vsize            int32    `json:"vsize"`
-	Weight           int32    `json:"weight"`
-	Fee              float64  `json:"fee"`
-	Time             int64    `json:"time"`
-	Height           int64    `json:"height"`
-	StartingPriority float64  `json:"startingpriority"`
-	CurrentPriority  float64  `json:"currentpriority"`
-	Depends          []string `json:"depends"`
-}
-
 // GetBlockVerboseResult models the data from the getblock command when the
 // verbose flag is set to 1.  When the verbose flag is set to 0, getblock returns a
 // hex-encoded string. When the verbose flag is set to 1, getblock returns an object
@@ -131,6 +110,36 @@ type GetBlockVerboseTxResult struct {
 	PreviousHash  string        `json:"previousblockhash"`
 	NextHash      string        `json:"nextblockhash,omitempty"`
 }
+
+
+//createMultisigresult models the data returned from the creatmulgising
+//command
+type CreateMultiSigResult struct {
+	Address string `json:"address"`
+	RedeemScript string `json:"redeem_script"`
+}
+
+
+
+
+
+
+
+// GetRawMempoolVerboseResult models the data returned from the getrawmempool
+// command when the verbose flag is set.  When the verbose flag is not set,
+// getrawmempool returns an array of transaction hashes.
+type GetRawMempoolVerboseResult struct {
+	Size             int32    `json:"size"`
+	Vsize            int32    `json:"vsize"`
+	Weight           int32    `json:"weight"`
+	Fee              float64  `json:"fee"`
+	Time             int64    `json:"time"`
+	Height           int64    `json:"height"`
+	StartingPriority float64  `json:"startingpriority"`
+	CurrentPriority  float64  `json:"currentpriority"`
+	Depends          []string `json:"depends"`
+}
+
 
 //vout models parts of the tx data.it is defined separately since both
 //getrawtrnasaxtion and decoderawtransaction use the same strutere.
