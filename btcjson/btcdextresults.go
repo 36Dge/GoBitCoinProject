15c8db319675af1bd27ff5577e5dee175bf73303
36dge <695931535@qq.com>
2022-01-07 21:19:18 +0800

    create btcdexresults file and define VersionResult struct

---
+++ btcjson/btcdextresults.go
@@ -0,0 +1,12 @@
+package btcjson
+
+//versionresult models object included in the version response ,in the actual
+//result,these objects are keyed by the program or API name.
+
+type VersionResult struct {
+	VersionString string `json:"versionstring"`
+	Major         uint32 `json:"major"`
+	Path          uint32 `json:"path"`
+	Prerelease    string `json:"prerelease"`
+	BuildMetadata string `json:"build_metadata"`
+}
